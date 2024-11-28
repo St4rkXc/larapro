f7a6bea41034403725dcb8de608875a4d8285915
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-11-28 10:03:56 +0800

    migration study

--- resources/views/components/navbar/index.blade.php
+++ resources/views/components/navbar/index.blade.php
@@ -22,7 +22,6 @@
                     </div>
                 </div>
             </div>
-
             <div class="-mr-2 flex md:hidden">
                 <!-- Mobile menu button -->
                 <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
